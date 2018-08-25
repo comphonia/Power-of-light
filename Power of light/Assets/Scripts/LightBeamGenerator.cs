e438fcca8255115d2c485fb0649a4915404459e7
Grendelbiter <m_degeest@protonmail.com>
2018-08-25 18:12:14 +0200

    generator and mirror

---
+++ Power of light/Assets/Scripts/LightBeamGenerator.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightBeamGenerator : MonoBehaviour
+{
+    public Transform rayOrigin;
+
+    public float maxDistance;
+
+    public bool drawRay;
+
+	void Update ()
+    {
+        RaycastHit hit;
+        if (Physics.Raycast(rayOrigin.position, rayOrigin.TransformDirection(Vector3.forward), out hit, maxDistance))
+        {
+            if (drawRay)
+            {
+                Debug.DrawRay(rayOrigin.position, rayOrigin.TransformDirection(Vector3.forward) * hit.distance, Color.white);
+            }
+            
+        }
+        else
+        {
+            if (drawRay)
+            {
+                Debug.DrawRay(rayOrigin.position, rayOrigin.TransformDirection(Vector3.forward) * maxDistance, Color.white);
+            }            
+        }
+
+    }
+}
