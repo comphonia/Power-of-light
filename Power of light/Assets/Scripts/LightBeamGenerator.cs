b9996d2184aa1aa606ce2bbd021ffd628aa95b57
Grendelbiter <m_degeest@protonmail.com>
2018-08-25 20:48:34 +0200

    reflection

--- Power of light/Assets/Scripts/LightBeamGenerator.cs
+++ Power of light/Assets/Scripts/LightBeamGenerator.cs
@@ -8,26 +8,41 @@ public class LightBeamGenerator : MonoBehaviour
 
     public float maxDistance;
 
+    public bool isPowered;
+
     public bool drawRay;
+    private Reflect reflect;
 
-	void Update ()
+    void Update ()
     {
-        RaycastHit hit;
-        if (Physics.Raycast(rayOrigin.position, rayOrigin.TransformDirection(Vector3.forward), out hit, maxDistance))
+        if (isPowered)
         {
-            if (drawRay)
+            RaycastHit hit;
+            if (Physics.Raycast(rayOrigin.position, rayOrigin.TransformDirection(Vector3.forward), out hit, maxDistance))
             {
-                Debug.DrawRay(rayOrigin.position, rayOrigin.TransformDirection(Vector3.forward) * hit.distance, Color.white);
-            }
-            
-        }
-        else
-        {
-            if (drawRay)
-            {
-                Debug.DrawRay(rayOrigin.position, rayOrigin.TransformDirection(Vector3.forward) * maxDistance, Color.white);
-            }            
-        }
+                if (hit.collider.CompareTag("Mirror"))
+                {
+                    Vector3 incomingVect = hit.point - rayOrigin.position;
+                    Vector3 reflectVect = Vector3.Reflect(incomingVect, hit.normal);
+                    reflect = hit.collider.gameObject.GetComponent<Reflect>();
+                    reflect.reflectVect = reflectVect;
+                    Debug.Log(reflectVect);
+                    reflect.hitPosition = hit.point;
+                    reflect.ReflectBeam();
+                }
+                if (drawRay)
+                {
+                    Debug.DrawRay(rayOrigin.position, rayOrigin.TransformDirection(Vector3.forward) * hit.distance, Color.white);
+                }
 
+            }
+            else
+            {
+                if (drawRay)
+                {
+                    Debug.DrawRay(rayOrigin.position, rayOrigin.TransformDirection(Vector3.forward) * maxDistance, Color.white);
+                }
+            }
+        }
     }
 }
---
+++ Power of light/Assets/Scripts/Reflect.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Reflect : MonoBehaviour
+{
+    public Vector3 reflectVect;
+
+    public Vector3 hitPosition;
+
+    public float maxLength;
+
+    RaycastHit hit;
+
+    private Reflect reflect;
+
+    public void ReflectBeam()
+    {
+        if (Physics.Raycast(hitPosition, reflectVect, out hit, maxLength))
+        {
+            if (hit.collider.CompareTag("Mirror"))
+            {
+                Vector3 incomingVect = hit.point - hitPosition;
+                Vector3 reflectVect = Vector3.Reflect(incomingVect, hit.normal);
+                reflect = hit.collider.gameObject.GetComponent<Reflect>();
+                reflect.reflectVect = reflectVect;
+                reflect.hitPosition = hit.point;
+                reflect.ReflectBeam();
+            }
+            Debug.DrawRay(hitPosition, reflectVect.normalized * hit.distance, Color.white);
+        }
+        else
+        {
+            Debug.DrawRay(hitPosition, reflectVect * maxLength, Color.white);
+        } 
+        
+    }
+}
+
