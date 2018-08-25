0e4a13c86ed1e77b75b309e95ece98089c46396f
Looooord <41568059+Looooord@users.noreply.github.com>
2018-08-25 14:58:40 +0200

    //

---
+++ Power of light/Assets/Enemy.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour {
+
+    private float health; 
+    public float Health
+    {
+        get
+        {
+            return health;
+        }
+        set
+        {
+            health = value;
+            if (health <= 0)
+            {
+                //Die
+            }
+        }
+    }
+    public float maxHealth;
+    public float defaultSpeed;
+
+    Vector3 direction; 
+    Transform target;
+    int pointIndex = 0;
+
+    private void Awake()
+    {
+        target = Waypoints.points[pointIndex];
+        transform.LookAt(target);
+        
+    }
+
+    private void Update()
+    {
+        direction = target.transform.position - transform.position;
+        transform.Translate(direction.normalized * defaultSpeed * Time.deltaTime, Space.World); 
+        if (Vector3.Distance(target.transform.position, transform.position) <= 0.2f) GetNewWaypoint(); 
+    }
+
+    void GetNewWaypoint()
+    {
+        if (pointIndex == Waypoints.points.Length - 1)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            pointIndex++;
+            target = Waypoints.points[pointIndex];
+            transform.LookAt(target);
+            direction = target.transform.position - transform.position;
+        }
+    }
+
+
+}
---
+++ Power of light/Assets/Wave.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Wave : MonoBehaviour {
+
+    int waveNumber = 1;
+    [SerializeField] GameObject[] enemyTypes;
+    [SerializeField] float[] probs;
+    [SerializeField] float spawnDelay; 
+    
+
+    private void Awake()
+    {
+        if (PlayerPrefs.HasKey("waveNumber")) waveNumber = PlayerPrefs.GetInt("waveNumber");
+        probs[0] = 100; 
+    }
+  
+    IEnumerator Spawning ()
+    {
+        int enemyType = ChooseEnemyType();
+        yield return new WaitForSeconds(spawnDelay);
+        Instantiate(enemyTypes[enemyType], transform.position, Quaternion.identity, transform); 
+        yield break; 
+    }
+
+    int ChooseEnemyType()
+    {
+        float number = Random.Range(0, 100);
+        float line = 0;
+        for (int i = 0; i < probs.Length; i++)
+        {
+            line += probs[i];
+            if (number < line) return i;
+        }
+        Debug.LogError("Enemy type not chosen");
+        return -1;
+    }
+
+}
