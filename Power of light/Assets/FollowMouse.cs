68b4301a1598927de99f3959f1e5de86dc8d63f0
Grendelbiter <m_degeest@protonmail.com>
2018-08-28 19:44:17 +0200

    dark scene and light follow script

---
+++ Power of light/Assets/FollowMouse.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowMouse : MonoBehaviour
+{
+    [SerializeField] LayerMask whereToPlace;
+
+    RaycastHit hit;
+
+	void Update ()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        if (Physics.Raycast(ray, out hit, Mathf.Infinity, whereToPlace ))
+        {
+            transform.position = hit.point;
+            transform.rotation = Quaternion.FromToRotation(Vector3.up, hit.normal);
+        }
+            
+	}
+}
