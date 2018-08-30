0bb2c15f2457510688aa53420cb4ef9b8401ada9
Looooord <41568059+Looooord@users.noreply.github.com>
2018-08-30 20:08:11 +0200

    Fixed rotation

--- Power of light/Assets/Scripts/GameScene/PlacementController.cs
+++ Power of light/Assets/Scripts/GameScene/PlacementController.cs
@@ -77,7 +77,7 @@ public class PlacementController : MonoBehaviour
     private void RotateFromMouseWheel()
     {
         //Debug.Log(Input.mouseScrollDelta);
-        mouseWheelRotation += Input.mouseScrollDelta.y;
+        mouseWheelRotation = Input.mouseScrollDelta.y;
         currentPlaceableObject.transform.Rotate(Vector3.up, mouseWheelRotation * 10f);
     }
 
