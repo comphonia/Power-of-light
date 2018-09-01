38c07ce81d04a1586fb1fa19995599ff5ad50c14
Grendelbiter <m_degeest@protonmail.com>
2018-09-01 03:06:45 +0200

    random death sounds

---
+++ Power of light/Assets/ParticleDestroy.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParticleDestroy : MonoBehaviour
+{
+    public AudioSource[] growls;
+
+    private int growlSound;
+
+    private void Start()
+    {
+        growlSound = Random.Range(0, growls.Length);
+        growls[growlSound].Play();
+        Destroy(gameObject, 5);
+    }
+
+}
